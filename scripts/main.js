c25fd62afc5f62c2ac0e48f042d3f5b45fd9015b
nawar988 <82041779+nawar988@users.noreply.github.com>
2021-04-15 05:09:38 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
 	let mySrc = myImage.getAttribute('src');
 	if (mySrc === 'images/Ann2.jpg') {
-		myImage.setAttribute('src','images/anger1');
+		myImage.setAttribute('src','images/anger1.jpg);
 	}
 	else {
-		myImage.setAttribute('src','images/Ann2')
+		myImage.setAttribute('src','images/Ann2.jpg');
 	}
 }
