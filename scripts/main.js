6038fed2c9d530a4f9feba4176f1f3a627c6fdb9
nawar988 <82041779+nawar988@users.noreply.github.com>
2021-04-15 05:12:03 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
-document.querySelector('img').onclick = function() {
-    alert("Don't touch my baby!");
-}
+
 let myImage = document.querySelector('img');
 myImage.onclick = function() {
 	let mySrc = myImage.getAttribute('src');
 	if (mySrc === 'images/Ann2.jpg') {
 		myImage.setAttribute('src','images/anger1.jpg');
+		alert("Don't touch my baby!");
 	}
 	else {
 		myImage.setAttribute('src','images/Ann2.jpg');
