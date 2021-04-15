a33b938daa3ceec4035fcec5e370a9b17993dafe
nawar988 <82041779+nawar988@users.noreply.github.com>
2021-04-15 05:07:06 +0300

    an

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 document.querySelector('img').onclick = function() {
     alert("Don't touch my baby!");
 }
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/Ann2.jpg') {
+		myImage.setAttribute('src','images/anger1');
+	}
+	else {
+		myImage.setAttribute('src','Ann2');
+	}
+}
